c91caf2af72dac2ba0ffa3b7c7b7a61dbacdfa30
HongyuZhao02 <hongyuzhao02@gmail.com>
2025-04-21 02:27:02 -0700

    updated code to write to display from touch array

--- Hongyu_Code/src/main.cpp
+++ Hongyu_Code/src/main.cpp
@@ -3,29 +3,43 @@
 #include "epd7in5_V2.h"
 #include "imagedata.h"
 #include "touch.h"
+#include "epdpaint.h"
 
-Touch touch;
+int counter = 0;
+int x_offset = 500;
+int y_offset = 500;
+int x_prev = 0;
+int y_prev = 0;
+int radius = 16; 
+
+unsigned char* image = (unsigned char *)malloc(EPD_WIDTH * EPD_HEIGHT / 8);
+unsigned char* circle = (unsigned char*)malloc((2*radius + 1)*(2*radius + 1)/8);
+Paint paint(image, EPD_WIDTH, EPD_HEIGHT);
+Paint circ(circle, 2*radius+1, 2*radius+1);
+
+Touch touch; 
+Epd epd;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  delay(1000);
+  delay(500);
 
-  
-  Epd epd;
+ 
   Serial.print("e-Paper init \r\n ");
   if (epd.Init() != 0) {
       Serial.print("e-Paper init failed\r\n ");
       return;
   }
 
-  delay(1000);
+  delay(500);
 
   Serial.print("e-Paper Display\r\n ");
   epd.Displaypart(IMAGE_DATA,250, 200,240,103);
   
   Serial.print("Displayed\r\n ");
 
-  delay(5000);
+  delay(1000);
 
   Serial.print("e-Paper Clear\r\n ");
   epd.Clear();
@@ -35,17 +49,42 @@ void setup() {
   Serial.print("End of setup function\r\n ");
   
   touch.touch_init();
-  
+  circ.DrawFilledCircle(radius+1, radius+1, radius, 0);
+
+  epd.Init();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int x_pos = touch.get_X_position();
   int y_pos = touch.get_Y_position();
+
+  if(x_pos > EPD_WIDTH){
+    x_pos = EPD_WIDTH;
+  }
+  if(y_pos > EPD_HEIGHT){
+    y_pos = EPD_HEIGHT;
+  }
+
   //if (MAP_POSITION_VALUES == false) {
     Serial.print(x_pos);
     Serial.print(",");
     Serial.println(y_pos);
   //}
+
+  if(x_pos != 0 && (x_pos != x_prev || y_pos != y_prev)){
+    paint.DrawFilledCircle(x_pos-x_offset, y_pos-y_offset, radius, 0);
+    epd.Displaypart(circle, x_pos-x_offset-radius, y_pos-y_offset-radius, 2*radius+1, 2*radius+1);
+    x_prev = x_pos;
+    y_prev = y_pos;
+    counter++;
+  }
+
+  if(counter > 10){
+    epd.Clear();
+    epd.Displaypart(image, 0, 0, EPD_WIDTH, EPD_HEIGHT);
+    counter = 0;
+  }
+
 
 }
