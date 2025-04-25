e2fd7edc65a8ea3d5aaf19b5450208f68e5c2350
junesha0 <juneshao@usc.edu>
2025-04-24 17:11:54 -0700

    wertyuio

---
+++ Code/JuneTriesToRunArduin/main/main.ino
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include "epd7in5_V2.h"
+#include "imagedata.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  
+  Epd epd;
+  Serial.print("e-Paper init \r\n ");
+  if (epd.Init() != 0) {
+      Serial.print("e-Paper init failed\r\n ");
+      return;
+  }
+
+  delay(1000);
+
+  Serial.print("e-Paper Display\r\n ");
+  epd.Displaypart(IMAGE_DATA,250, 200,240,103);
+  //epd.Displaypart(IMAGE_DATA,250, 200,240,50);
+  // epd.Displaypart(IMAGE_DATA,0, 0,800,480);
+
+  Serial.print("Displayed\r\n ");
+
+  delay(5000);
+
+  Serial.print("e-Paper Clear\r\n ");
+  //epd.Clear();
+
+  epd.Sleep();
+
+  Serial.print("End of setup function\r\n ");
+}
+
+void update_screen(Epd epd) {
+  Serial.print("New Data\r\n ");
+  // epd.Displaypart(IMAGE_DATA,250, 200,240,103);
+  epd.Displaypart(IMAGE_DATA,0, 0,800,480);
+
+  Serial.print("Displayed\r\n ");
+
+  epd.Sleep();
+
+  Serial.print("Sleeping... zzz... ");
+}
+
+void loop() {
+
+}
